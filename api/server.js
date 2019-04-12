c6b489fd2ae162e8e06d64d30cd596f6099f9c64
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-12 10:10:35 -0700

    get-test-made-first

--- api/server.js
+++ api/server.js
@@ -1,28 +1,27 @@
 //express for server
-const express = require('express');
+const express = require("express");
 //database model
-const gamesDB = require('../games/gamesModel.js');
+const gamesDB = require("../games/gamesModel.js");
 //server uses express
 const server = express();
 //JSON through express for server
 server.use(express.json());
 //ROOT of API
-server.get('/', async (req, res) => {
-    res.status(200).json({ api: 'running' });
-})
+server.get("/", async (req, res) => {
+  res.status(200).json({ api: "running" });
+});
 //GAMES CRUD
 //POST
-server.post('/games', async (req, res)=>{
-    let body = req.body;
-    if(body.title && body.genre){
-        gamesDB.insert(body);
-        res.status(201).json(body.id);
-    }
-    else{
-        res.status(422).json({
-            errorMessage: 'Failed to add game'
-        })
-    }
-})
+server.post("/games", async (req, res) => {
+  let body = req.body;
+  if (body.title && body.genre) {
+    gamesDB.insert(body);
+    res.status(201).json({message: 'game added'});
+  } else {
+    res.status(422).json({
+      errorMessage: "Failed to add game"
+    });
+  }
+});
 //CONNECT to server by Export!a
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,13 +1,38 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const server = require('./server.js');
+const server = require("./server.js");
 
-describe('Routes', () => {
-    describe('POST /games', () => {
-        it('should post and return status 201', async ()=>{
-            const body = {title: 'Pacman', genre: 'Arcade', releaseYear: 1980}
-            const res = await request(server).post('/games').send(body);
-            expect(res.status).toBe(201);
-        })
+describe("Routes", () => {
+  describe("POST /games", () => {
+    it("should post and return status 201", async () => {
+      const body = { title: "Pacman", genre: "Arcade", releaseYear: 1980 };
+      const res = await request(server)
+        .post("/games")
+        .send(body);
+      expect(res.status).toBe(201);
+    });
+    it("should return post failure status 422", async () => {
+      const body = { title: "Quack", releaseYear: 2000 };
+      const res = await request(server)
+        .post("/games")
+        .send(body);
+      expect(res.status).toBe(422);
+    });
+    it("should return success message", async () => {
+      const body = { title: "Quack", genre: "swim", releaseYear: 2000 };
+      const res = await request(server)
+        .post("/games")
+        .send(body);
+      expect(res.body).toEqual({
+        message: "game added"
+      });
+    });
+  });
+
+  describe('GET /games', () => {
+    it('should return status 200 on get request', async ()=>{
+        const res = await request(server).get('/games');
+        expect(res.status).toBe(200);
     })
-})
+  })
+});
